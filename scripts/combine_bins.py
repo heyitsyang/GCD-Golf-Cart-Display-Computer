fae42e41717a6f32b0615fc6d2cdf9040a9d12ef
Yangmin Shen <14240918+heyitsyang@users.noreply.github.com>
2025-11-28 09:25:56 -0500

    change instructions for flashing

--- scripts/combine_bins.py
+++ scripts/combine_bins.py
@@ -68,7 +68,8 @@ def create_factory_bin(*args, **kwargs):
         factory_size = os.path.getsize(factory_bin_path)
         print(f"\nSuccessfully created factory image: {factory_bin_path}")
         print(f"Factory image size: {factory_size} bytes ({factory_size / 1024 / 1024:.2f} MB)")
-        print(f"Use offset of 0x0000 when flashing")
+        print(f"Use offset of 0x0000 when flashing {os.path.basename(factory_bin_path)}")
+        print(f"Use offset of 0x10000 when flashing firmware.bin")        
     except subprocess.CalledProcessError as e:
         print(f"\nFailed to create factory image. Error: {e}")
     except FileNotFoundError:
