64de76f38a8e9b33929831f1a4cecd1e4fbe09d7
Yangmin Shen <14240918+heyitsyang@users.noreply.github.com>
2026-01-16 20:30:22 -0500

    rm DEMO_MODE

--- src/utils/sleep_manager.cpp
+++ src/utils/sleep_manager.cpp
@@ -53,21 +53,12 @@ void initSleepPin() {
     Serial.print("Sleep pin (GPIO ");
     Serial.print(SLEEP_PIN);
     Serial.println(") initialized as INPUT_PULLUP");
-#ifdef DEMO_MODE
-    Serial.println("*** DEMO MODE: Sleep functionality DISABLED ***");
-#else
     Serial.println("Device will enter deep sleep when pin is LOW, reboot when HIGH");
-#endif
 }
 
 bool shouldEnterSleep() {
-#ifdef DEMO_MODE
-    // Demo mode: never enter sleep, ignore SLEEP_PIN
-    return false;
-#else
     // Return true if SLEEP_PIN is LOW (pulled to ground)
     return digitalRead(SLEEP_PIN) == LOW;
-#endif
 }
 
 void enterDeepSleep() {
@@ -202,11 +193,6 @@ static bool isGciActivelyConnected() {
  * Returns true if deep sleep should be entered
  */
 bool processSleepModeStateMachine() {
-#ifdef DEMO_MODE
-    // Demo mode: never enter sleep
-    return false;
-#endif
-
     uint32_t now = millis();
     uint32_t timeout_ms = (uint32_t)backlight_timeout * 60UL * 1000UL;
 
